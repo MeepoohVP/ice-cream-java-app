e03f175c3d83c388fa43c9b5ed916b470fceef4e
poohvp <poohvp1147@gmail.com>
2024-06-07 22:16:15 +0700

    update ui

--- src/main/java/ui/App.java
+++ src/main/java/ui/App.java
@@ -10,23 +10,23 @@ import java.util.Scanner;
 import java.util.stream.Stream;
 
 public class App {
+    private static MemCustomerRepository c = new MemCustomerRepository();
+    private static MemOrderRepository o = new MemOrderRepository();
+    private static MemOrderDetailRepository od = new MemOrderDetailRepository();
+    private static ShopService sh = new ShopService(c, o, od);
     private static void receiveOrder() {
-        MemCustomerRepository c = new MemCustomerRepository();
-        MemOrderRepository o = new MemOrderRepository();
-        MemOrderDetailRepository od = new MemOrderDetailRepository();
-        ShopService sh = new ShopService(c, o, od);
         while (true){
             System.out.println("Menu");
             Stream.of("Banana split 99.-","strawberry sundae 169.-",
                     "rocky road 129.-","cookie and cream cone 69.-",
                     "vanilla cone 69.-").forEach(System.out::println);
             Scanner sc = new Scanner(System.in);
-            Customer cust = sh.registerCustomer();
             System.out.print("What would you like to order? (exit or q to back to option page): ");
             String menuName = sc.nextLine();
             if (menuName.matches("q|exit")) {
                 break;
             }
+            Customer cust = sh.registerCustomer();
             System.out.print("How many cups do you need?: ");
             Order order = sh.addOrder(cust.getQueue());
             sh.createOrderDetail(order.getCode());
@@ -62,6 +62,61 @@ public class App {
             System.out.println(order.getStatus());
         }
     }
+    public static void manage(){
+        while (true){
+            System.out.println("In this option you can manage" +
+                    "\n- customer" +
+                    "\n- order" +
+                    "\n- order detail");
+            Scanner sc = new Scanner(System.in);
+            System.out.print("What will you manage?(exit or q to back to option page): ");
+            String m = sc.nextLine();
+            if (m.matches("q|exit")) {
+                break;
+            }
+            if (m.equals("customer")) {
+                System.out.print("you want to find or list all customer:");
+                String select = sc.nextLine();
+                if (select.equals("find")){
+                    System.out.print("Enter customer queue: ");
+                    String queue = sc.nextLine();
+                    System.out.println(sh.findCustomer(queue));
+                } else if (select.equals("list all")) {
+                    System.out.println(sh.allCustomers());
+                } else {
+                    System.out.println("Invalid option");
+                }
+            } else if (m.equals("order")) {
+                System.out.print("you want to find, update or list all order: ");
+                String select = sc.nextLine();
+                if (select.equals("find")){
+                    System.out.print("Enter order code: ");
+                    String code = sc.nextLine();
+                    System.out.println(sh.findOrder(code));
+                } else if (select.equals("update")) {
+                    System.out.print("Enter order code: ");
+                    String code = sc.nextLine();
+                    System.out.println(sh.updateOrder(sh.findOrder(code)));
+                } else if (select.equals("list all")) {
+                    System.out.println(sh.allOrders());
+                } else {
+                    System.out.println("Invalid option");
+                }
+            } else if (m.equals("order detail")) {
+                System.out.print("you want to find or list all order: ");
+                String select = sc.nextLine();
+                if (select.equals("find")){
+                    System.out.print("Enter order code: ");
+                    String code = sc.nextLine();
+                    System.out.println(sh.findOrderDetail(code));
+                } else if (select.equals("list all")) {
+                    System.out.println(sh.allOrderDetails());
+                }
+            }else {
+                System.out.println("Invalid option");
+            }
+        }
+    }
     public static void start(){
         Console cs = System.console();
         String user =  cs.readLine("Enter your username: ");
@@ -73,11 +128,14 @@ public class App {
         System.out.println("Welcome to Ice-cream shop!");
         while (pw.equals("icecream123")) {
             Scanner input = new Scanner(System.in);
-            //receive order, showDetail
+            //receive order, manage
             System.out.print("Choose option: ");
             String option = input.nextLine();
             if(option.equals("receive order")){
                 App.receiveOrder();
+            }
+            if(option.equals("manage")){
+                App.manage();
             }
         }
     }
